9a18aad30d7c686c5a81468a42319e8b03fb3174
Kevin Chen <kevinmichaelchen@gmail.com>
2022-05-18 22:44:24 -0400

    test

--- internal/service/ranking/rank_trips_test.go
+++ internal/service/ranking/rank_trips_test.go
@@ -10,20 +10,40 @@ import (
 )
 
 func TestScoreTrip(t *testing.T) {
-	actual := scoreTrip(&v1beta1.SearchResult{
+	tests := map[string]struct {
+		timeUntilTripStart    time.Duration
+		timeForDriverToArrive time.Duration
+		payment               *v1beta1.Money
+		expected              float64
+	}{
+		"OK": {
+			timeUntilTripStart:    5 * time.Minute,
+			timeForDriverToArrive: time.Minute + (15 * time.Second),
+			payment:               newMoney(15, 50),
+			expected:              109.5,
+		},
+	}
+	for testName, tc := range tests {
+		t.Run(testName, func(t *testing.T) {
+			sr := newSearchResult(tc.timeUntilTripStart, tc.timeForDriverToArrive, tc.payment)
+			actual := scoreTrip(sr)
+			require.Equal(t, tc.expected, actual)
+		})
+	}
+
+}
+
+func newSearchResult(timeUntilTripStart time.Duration, timeForDriverToArrive time.Duration, payment *v1beta1.Money) *v1beta1.SearchResult {
+	return &v1beta1.SearchResult{
 		Payload: &v1beta1.SearchResult_Trip{
 			Trip: &v1beta1.Trip{
-				Id:           "trip1",
-				ScheduledFor: timestamppb.New(time.Now().Add(5 * time.Minute)),
-				ExpectedPayment: &v1beta1.Money{
-					CurrencyCode: "USD",
-					Units:        15,
-					Nanos:        50 * 10_000_000,
-				},
+				Id:              "trip",
+				ScheduledFor:    timestamppb.New(time.Now().Add(timeUntilTripStart)),
+				ExpectedPayment: payment,
 			},
 		},
 		DistanceMeters: 100,
-		Duration:       durationpb.New(time.Minute + (15 * time.Second)),
+		Duration:       durationpb.New(timeForDriverToArrive),
 		Location: &v1beta1.LatLng{
 			Latitude:  10,
 			Longitude: 10,
@@ -31,6 +51,13 @@ func TestScoreTrip(t *testing.T) {
 		Address:    "Address",
 		Resolution: 8,
 		KValue:     2,
-	})
-	require.Equal(t, float64(109.5), actual)
+	}
+}
+
+func newMoney(units int64, cents int32) *v1beta1.Money {
+	return &v1beta1.Money{
+		CurrencyCode: "USD",
+		Units:        units,
+		Nanos:        cents * 10_000_000,
+	}
 }
