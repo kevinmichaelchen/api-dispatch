cd08844c77ab00b560a0261efba478bebc026e61
Kevin Chen <kevinmichaelchen@gmail.com>
2022-05-31 14:26:15 -0400

    kafka

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module github.com/kevinmichaelchen/api-dispatch
 go 1.18
 
 require (
+	github.com/Shopify/sarama v1.34.0
 	github.com/XSAM/otelsql v0.14.1
 	github.com/codingsince1985/geo-golang v1.8.1
 	github.com/envoyproxy/protoc-gen-validate v0.1.0
@@ -12,7 +13,7 @@ require (
 	github.com/google/go-cmp v0.5.8
 	github.com/grpc-ecosystem/go-grpc-middleware v1.3.0
 	github.com/kat-co/vala v0.0.0-20170210184112-42e1d8b61f12
-	github.com/kr/pretty v0.2.0
+	github.com/kr/pretty v0.3.0
 	github.com/lib/pq v1.10.5
 	github.com/paulmach/go.geo v0.0.0-20180829195134-22b514266d33
 	github.com/rs/xid v1.4.0
@@ -47,6 +48,9 @@ require (
 	github.com/beorn7/perks v1.0.1 // indirect
 	github.com/cespare/xxhash/v2 v2.1.2 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
+	github.com/eapache/go-resiliency v1.2.0 // indirect
+	github.com/eapache/go-xerial-snappy v0.0.0-20180814174437-776d5712da21 // indirect
+	github.com/eapache/queue v1.1.0 // indirect
 	github.com/ericlagergren/decimal v0.0.0-20181231230500-73749d4874d5 // indirect
 	github.com/felixge/httpsnoop v1.0.2 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
@@ -54,20 +58,33 @@ require (
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/gofrs/uuid v3.2.0+incompatible // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
+	github.com/golang/snappy v0.0.4 // indirect
 	github.com/google/uuid v1.3.0 // indirect
+	github.com/hashicorp/errwrap v1.0.0 // indirect
+	github.com/hashicorp/go-multierror v1.1.1 // indirect
+	github.com/hashicorp/go-uuid v1.0.2 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
-	github.com/kr/text v0.1.0 // indirect
+	github.com/jcmturner/aescts/v2 v2.0.0 // indirect
+	github.com/jcmturner/dnsutils/v2 v2.0.0 // indirect
+	github.com/jcmturner/gofork v1.0.0 // indirect
+	github.com/jcmturner/gokrb5/v8 v8.4.2 // indirect
+	github.com/jcmturner/rpc/v2 v2.0.3 // indirect
+	github.com/klauspost/compress v1.15.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/magiconair/properties v1.8.5 // indirect
 	github.com/matttproud/golang_protobuf_extensions v1.0.1 // indirect
 	github.com/mitchellh/mapstructure v1.4.2 // indirect
 	github.com/paulmach/go.geojson v1.4.0 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
+	github.com/pierrec/lz4/v4 v4.1.14 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
 	github.com/prometheus/client_golang v1.12.1 // indirect
 	github.com/prometheus/client_model v0.2.0 // indirect
 	github.com/prometheus/common v0.32.1 // indirect
 	github.com/prometheus/procfs v0.7.3 // indirect
+	github.com/rcrowley/go-metrics v0.0.0-20201227073835-cf1acfcdf475 // indirect
+	github.com/rogpeppe/go-internal v1.6.1 // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
@@ -78,7 +95,8 @@ require (
 	go.uber.org/atomic v1.9.0 // indirect
 	go.uber.org/dig v1.14.1 // indirect
 	go.uber.org/multierr v1.8.0 // indirect
-	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/crypto v0.0.0-20220214200702-86341886e292 // indirect
+	golang.org/x/net v0.0.0-20220520000938-2e3eb7b945c2 // indirect
 	golang.org/x/sys v0.0.0-20220429233432-b5fbb4746d32 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	golang.org/x/time v0.0.0-20200416051211-89c76fbcd5d1 // indirect
---
+++ internal/app/kafka/kafka.go
@@ -0,0 +1,186 @@
+package kafka
+
+// SIGUSR1 toggle the pause/resume consumption
+import (
+	"context"
+	"github.com/Shopify/sarama"
+	"go.uber.org/fx"
+	"go.uber.org/zap"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
+	"sync"
+	"syscall"
+)
+
+// Sarama configuration options
+var (
+	brokers  = ""
+	version  = ""
+	group    = ""
+	topics   = ""
+	assignor = ""
+	oldest   = true
+	verbose  = false
+)
+
+var Module = fx.Module("kafka",
+	fx.Provide(
+		NewConsumerGroup,
+	),
+)
+
+// NewConsumerGroup creates a new Kafka consumer group.
+// Inspired by https://github.com/Shopify/sarama/blob/main/examples/consumergroup/main.go
+func NewConsumerGroup(logger *zap.Logger, lc fx.Lifecycle) (sarama.ConsumerGroup, error) {
+	keepRunning := true
+	logger.Info("Starting a new Sarama consumer")
+	if verbose {
+		sarama.Logger = log.New(os.Stdout, "[sarama] ", log.LstdFlags)
+	}
+
+	version, err := sarama.ParseKafkaVersion(version)
+	if err != nil {
+		return nil, err
+	}
+
+	/**
+	 * Construct a new Sarama configuration.
+	 * The Kafka cluster version has to be defined before the consumer/producer is initialized.
+	 */
+	config := sarama.NewConfig()
+	config.Version = version
+
+	switch assignor {
+	case "sticky":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategySticky
+	case "roundrobin":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRoundRobin
+	case "range":
+		config.Consumer.Group.Rebalance.Strategy = sarama.BalanceStrategyRange
+	default:
+		log.Panicf("Unrecognized consumer group partition assignor: %s", assignor)
+	}
+
+	if oldest {
+		config.Consumer.Offsets.Initial = sarama.OffsetOldest
+	}
+
+	/**
+	 * Setup a new Sarama consumer group
+	 */
+	consumer := Consumer{
+		ready: make(chan bool),
+	}
+
+	var client sarama.ConsumerGroup
+
+	lc.Append(fx.Hook{
+		OnStart: func(ctx context.Context) error {
+			ctx, cancel := context.WithCancel(ctx)
+			defer cancel()
+
+			cc, err := sarama.NewConsumerGroup(strings.Split(brokers, ","), group, config)
+			if err != nil {
+				return err
+			}
+			client = cc
+
+			consumptionIsPaused := false
+			wg := &sync.WaitGroup{}
+			wg.Add(1)
+			go func() {
+				defer wg.Done()
+				for {
+					// `Consume` should be called inside an infinite loop, when a
+					// server-side rebalance happens, the consumer session will need to be
+					// recreated to get the new claims
+					if err := client.Consume(ctx, strings.Split(topics, ","), &consumer); err != nil {
+						logger.Panic("Error from consumer", zap.Error(err))
+					}
+					// check if context was cancelled, signaling that the consumer should stop
+					if ctx.Err() != nil {
+						return
+					}
+					consumer.ready = make(chan bool)
+				}
+			}()
+
+			<-consumer.ready // Await till the consumer has been set up
+			logger.Info("Sarama consumer up and running!...")
+
+			sigusr1 := make(chan os.Signal, 1)
+			signal.Notify(sigusr1, syscall.SIGUSR1)
+
+			for keepRunning {
+				select {
+				case <-ctx.Done():
+					logger.Info("terminating: context cancelled")
+					keepRunning = false
+				case <-sigusr1:
+					toggleConsumptionFlow(logger, client, &consumptionIsPaused)
+				}
+			}
+			wg.Wait()
+
+			return nil
+		},
+		OnStop: func(ctx context.Context) error {
+			logger.Info("Closing Kafka connection...")
+			err := client.Close()
+			if err != nil {
+				logger.Error("Failed to close Kafka connection", zap.Error(err))
+			} else {
+				logger.Info("Successfully closed Kafka connection")
+			}
+			return err
+		},
+	})
+
+	return client, nil
+}
+
+func toggleConsumptionFlow(logger *zap.Logger, client sarama.ConsumerGroup, isPaused *bool) {
+	if *isPaused {
+		client.ResumeAll()
+		logger.Info("Resuming consumption")
+	} else {
+		client.PauseAll()
+		logger.Info("Pausing consumption")
+	}
+
+	*isPaused = !*isPaused
+}
+
+// Consumer represents a Sarama consumer group consumer
+type Consumer struct {
+	ready chan bool
+}
+
+// Setup is run at the beginning of a new session, before ConsumeClaim
+func (consumer *Consumer) Setup(sarama.ConsumerGroupSession) error {
+	// Mark the consumer as ready
+	close(consumer.ready)
+	return nil
+}
+
+// Cleanup is run at the end of a session, once all ConsumeClaim goroutines have exited
+func (consumer *Consumer) Cleanup(sarama.ConsumerGroupSession) error {
+	return nil
+}
+
+// ConsumeClaim must start a consumer loop of ConsumerGroupClaim's Messages().
+func (consumer *Consumer) ConsumeClaim(session sarama.ConsumerGroupSession, claim sarama.ConsumerGroupClaim) error {
+	// NOTE:
+	// Do not move the code below to a goroutine.
+	// The `ConsumeClaim` itself is called within a goroutine, see:
+	// https://github.com/Shopify/sarama/blob/main/consumer_group.go#L27-L29
+	for message := range claim.Messages() {
+		// TODO convert bytes to protobuf
+		log.Printf("Message claimed: value = %s, timestamp = %v, topic = %s", string(message.Value), message.Timestamp, message.Topic)
+		session.MarkMessage(message, "")
+	}
+
+	return nil
+}
