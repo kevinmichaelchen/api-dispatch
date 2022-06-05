7449a088ff8a60d9ddbdfe27c8e6c6c684e5011e
Kevin Chen <kevinmichaelchen@gmail.com>
2022-06-05 00:21:37 -0400

    normalized state (#31)

--- ui/src/types.ts
+++ ui/src/types.ts
@@ -9,3 +9,40 @@ export interface DriverLocation {
   id: string;
   mostRecentHeartbeat: string;
 }
+
+export interface NormalizedDriverLocations {
+  byId: { [key: string]: DriverLocation };
+  allIds: string[];
+}
+
+export function driverLocationsToState(
+  driverLocations: DriverLocation[]
+): NormalizedDriverLocations {
+  return {
+    allIds: driverLocations.map((dl) => dl.driverId),
+    byId: driverLocations.reduce((acc: any, dl: DriverLocation) => {
+      acc[dl.driverId] = dl;
+      return acc;
+    }, {}),
+  };
+}
+
+export function addDriverLocationToState(
+  s: NormalizedDriverLocations,
+  dl: DriverLocation
+): NormalizedDriverLocations {
+  // prevent double-add
+  if (s?.byId?.[dl.driverId]) {
+    return s;
+  }
+  return {
+    byId: { ...s.byId, [dl.driverId]: dl },
+    allIds: [...s.allIds, dl.driverId],
+  } as NormalizedDriverLocations;
+}
+
+export function getDriverLocationsFromState(
+  s: NormalizedDriverLocations
+): DriverLocation[] {
+  return Object.entries(s?.byId ?? []).map(([id, dl], i: number) => dl);
+}
